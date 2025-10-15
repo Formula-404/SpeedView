ded0f858782d340d70da1de7ae6f82089b6e6a63
Helvennn <enixgregory@gmail.com>
2025-10-15 23:26:28 +0700

    feat/Team models added

--- apps/team/models.py
+++ apps/team/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    team_name = models.CharField(max_length=255, primary_key=True)
+    team_colour = models.CharField(max_length=6)
+    team_description = models.TextField(blank=True, null=True)
