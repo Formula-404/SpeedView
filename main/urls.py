f077db2d90f82088eb0e92dc32165b26da69767b
lucidd27212 <gilangadjiesaputra@gmail.com>
2025-10-24 11:50:33 +0700

    feat/ driver laps pit test

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-    # path('', show_main, name='show_main'),
+    #path('', show_main, name='show_main'),
     path("", views.main_dashboard_page, name="show_main"),
     path("api/recent-meetings/", views.api_recent_meetings, name="api_recent_meetings"),    
     path("api/dashboard-data/", views.api_dashboard_data, name="api_dashboard_data"),
