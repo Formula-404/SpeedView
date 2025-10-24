77490620d88d690481b7826ab2e5ed516c32f5b8
Helvennn <enixgregory@gmail.com>
2025-10-24 10:51:25 +0700

    feat/comparison done?

--- apps/comparison/models.py
+++ apps/comparison/models.py
@@ -31,3 +31,30 @@ class ComparisonTeam(models.Model):
     comparison = models.ForeignKey(Comparison, on_delete=models.CASCADE, related_name="team_links")
     team = models.ForeignKey("team.Team", on_delete=models.CASCADE)
     order_index = models.PositiveSmallIntegerField(default=0)
+
+class ComparisonCircuit(models.Model):
+    comparison = models.ForeignKey(Comparison, on_delete=models.CASCADE, related_name="circuit_links")
+    circuit = models.ForeignKey("circuit.Circuit", on_delete=models.CASCADE)
+    order_index = models.PositiveSmallIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.comparison.id} → {self.circuit.id} ({self.order_index})"
+    
+class ComparisonDriver(models.Model):
+    comparison = models.ForeignKey(Comparison, on_delete=models.CASCADE, related_name="driver_links")
+    driver = models.ForeignKey("driver.Driver", on_delete=models.CASCADE)
+    order_index = models.PositiveSmallIntegerField(default=0)
+
+
+    def __str__(self):
+        return f"{self.comparison.id} → {self.driver.driver_number} ({self.order_index})"
+
+
+class ComparisonCar(models.Model):
+    comparison = models.ForeignKey(Comparison, on_delete=models.CASCADE, related_name="car_links")
+    car = models.ForeignKey("car.Car", on_delete=models.CASCADE)
+    order_index = models.PositiveSmallIntegerField(default=0)
+
+
+    def __str__(self):
+        return f"{self.comparison.id} → {self.car} ({self.order_index})"
--- apps/comparison/urls.py
+++ apps/comparison/urls.py
@@ -5,9 +5,10 @@ app_name = "comparison"
 
 urlpatterns = [
     # API
-    path("api/list/", views.api_list, name="api_list"),
+    path("api/list/", views.api_comparison_list, name="api_list"),
     path("api/create/", views.api_comparison_create, name="api_create"),
     path("api/<uuid:pk>/", views.api_comparison_detail, name="api_detail"),
+    path("api/<uuid:pk>/delete/", views.api_comparison_delete, name="api_delete"),
 
     # Page
     path("", views.list_page, name="list_page"),
